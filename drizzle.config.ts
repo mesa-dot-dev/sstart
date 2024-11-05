9f23aa6a58a3ec4e9d775692f3ae8e0a2eb41be9
Taylor Steele <taylor.steele@disneystreaming.com>
2024-11-05 09:30:46 -0500

    try reject unauthorized

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,11 +5,15 @@ export default defineConfig({
   dialect: "postgresql",
   schema: ["./app/**/*.sql.ts"],
   out: "./app/database/migrations/",
+
   dbCredentials: {
     host: Resource.TSSDatabase.host,
     port: Resource.TSSDatabase.port,
     user: Resource.TSSDatabase.username,
     password: Resource.TSSDatabase.password,
     database: Resource.TSSDatabase.database,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 });
