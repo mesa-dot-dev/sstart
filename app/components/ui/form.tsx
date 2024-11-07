33f13b616cc868e38a076c08640e96046b1c0495
Taylor Steele <taylor.steele@disneystreaming.com>
2024-11-07 16:50:35 -0500

    I hate linting man

--- app/components/ui/form.tsx
+++ app/components/ui/form.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @eslint-react/no-unstable-context-value */
 import * as React from "react";
 import * as LabelPrimitive from "@radix-ui/react-label";
 import { Slot } from "@radix-ui/react-slot";
