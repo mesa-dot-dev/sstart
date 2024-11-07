e1f4e8ef8fb3af4d4f250a0798f14905f9fb2626
Taylor Steele <taylor.steele@disneystreaming.com>
2024-11-07 18:19:27 -0500

    trusted origins I guess

--- app/features/auth/lib/auth.ts
+++ app/features/auth/lib/auth.ts
@@ -11,4 +11,9 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  trustedOrigins: [
+    "https://d1p64y1v9iordo.cloudfront.net",
+    "https://dadc32hngl0az.cloudfront.net",
+    "https://d1l6gad6488u49.cloudfront.net",
+  ],
 });
